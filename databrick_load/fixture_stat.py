7ba5afd91d5813e072a7585ab82d1b7b0e115666
tallal-usman@outlook.com <tallal-usman@outlook.com>
2023-06-27 07:33:05 +0000

    fixture stat

---
+++ databrick_load/fixture_stat.py
@@ -0,0 +1,23 @@
+from pyspark.sql.functions import col, explode
+from pyspark.sql import functions as F
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+input_dir = "gs://football_data_api/raw/fixture-stats/2023-02-22/2023-02-22.json"
+
+# Load the entire file as a single record
+json = spark.sparkContext.wholeTextFiles(input_dir).values()
+df = spark.read.json(json)
+
+df1 = (df.select(col("Data.parameters.fixture").alias("fixture_id"), explode(col("Data.response")))
+               .withColumn('team_id', F.col('col.team.id'))
+               .withColumn('total_shots', F.col('col.statistics').getItem(2).getField('value'))
+               .withColumn('possession', F.col('col.statistics').getItem(9).getField('value'))
+               .withColumn('red_cards', F.col('col.statistics').getItem(11).getField('value'))
+               .withColumn('gk_saves', F.col('col.statistics').getItem(12).getField('value'))
+               .withColumn('fouls', F.col('col.statistics').getItem(6).getField('value'))
+               .withColumn('tot_passes', F.col('col.statistics').getItem(13).getField('value'))
+               .withColumn('a_passes', F.col('col.statistics').getItem(14).getField('value'))
+               .drop('col'))
+
+df1.write.format("parquet").mode("overwrite").saveAsTable("bronze_fixture_stat")
