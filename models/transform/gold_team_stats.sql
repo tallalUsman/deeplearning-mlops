8e923c00476257306a4a9ff447443e707bddc05c
Tallal Usman <tallal-usman@outlook.com>
2023-06-28 21:28:11 +0000

    adding more files

--- models/transform/gold_team_stats.sql
+++ models/transform/gold_team_stats.sql
@@ -1,11 +1,29 @@
 WITH fixture_dates AS (
-    SELECT DISTINCT fixture_date, fixture_id
+    SELECT DISTINCT fixture_date, fixture_id, season
     FROM {{ ref('silver_fixture') }}
 )
 
-SELECT silver_fixture_stat.* 
-, fixture_dates.fixture_date
+, standing AS (
+    SELECT DISTINCT season, team_id, rank AS prior_season_rank
+    FROM {{ ref('silver_standing') }}
+)
+
+, temp_view AS (
+    SELECT silver_fixture_stat.* 
+    , fixture_dates.fixture_date
+    , standing.prior_season_rank
 FROM {{ ref('silver_fixture_stat') }}
 INNER JOIN fixture_dates
-USING (fixture_id)
+    USING (fixture_id)
+INNER JOIN standing
+    ON standing.team_id = silver_fixture_stat.team_id AND fixture_dates.season = (standing.season - 1)
 ORDER BY team_id, fixture_date
+)
+
+SELECT 
+AVG(total_shots) OVER (
+        PARTITION BY team_id 
+        ORDER BY fixture_date DESC
+        ROWS BETWEEN 9 PRECEDING AND 1 PRECEDING
+    ) as moving_average
+FROM temp_view
--- models/transform/silver_coach.sql
+++ models/transform/silver_coach.sql
@@ -1,4 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT * FROM football.bronze_coach
+SELECT * 
+FROM football.bronze_coach
 WHERE team_id IS NOT NULL
