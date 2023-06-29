3aed71c27ab3d486adb7b3811dba8c1516d6fd5a
Tallal Usman <tallal-usman@outlook.com>
2023-06-29 07:11:11 +0000

    finalize DL gold

--- models/transform/gold_DL_table.sql
+++ models/transform/gold_DL_table.sql
@@ -18,11 +18,15 @@ SELECT silver_fixture.*
 , (away.gk_saves_avg/home.gk_saves_avg) AS gk_saves_r
 , (away.fouls_avg/home.fouls_avg) AS fouls_r
 , (away.pass_accuracy_avg/home.pass_accuracy_avg) AS pass_accuracy_r
+, away_coach.coach_id AS coach_id_away
+, home_coach.coach_id AS coach_id_home
 FROM {{ ref('silver_fixture') }}
 INNER JOIN team_lvl away
 ON silver_fixture.fixture_id = away.fixture_id AND silver_fixture.away_team_id = away.team_id 
 INNER JOIN team_lvl home
 ON silver_fixture.fixture_id = home.fixture_id AND silver_fixture.home_team_id = home.team_id 
-INNER JOIN {{ ref('silver_coach') }} away
-ON silver_coach.team_id = silver_fixture.away_team_id  AND silver_fixture.fixture_date BETWEEN silver_coach.start_date AND silver_coach.calc_end_date
+INNER JOIN {{ ref('silver_coach') }} away_coach
+ON away_coach.team_id = silver_fixture.away_team_id  AND silver_fixture.fixture_date BETWEEN away_coach.start_date AND away_coach.calc_end_date
+INNER JOIN {{ ref('silver_coach') }} home_coach
+ON home_coach.team_id = silver_fixture.home_team_id  AND silver_fixture.fixture_date BETWEEN home_coach.start_date AND home_coach.calc_end_date
 WHERE (away.total_shots_avg/home.total_shots_avg) IS NOT NULL
